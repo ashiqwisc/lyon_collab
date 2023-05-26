24c7b1465ed19aad24247346b7697021a041b12a
ashiqwisc <ashiq@wisc.edu>
2023-05-25 17:23:38 -0700

    First RQ for Model 0

---
+++ code/rq1_ona_plotter_lyon.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+
+make.ona.plot <- function(set, 
+                          plot_name, 
+                          flip_x = FALSE
+) {
+  mean <- plot(set, title = "Mean ONA plot") %>%
+    edges(
+      weights = set$line.weights,
+      edge_size_multiplier = 2,
+      edge_color = c("red")) %>%
+    nodes(
+      # node_size_multiplier = 0.5,
+      self_connection_color = c("red")) %>%
+    units(
+      points=set$points,
+      points_color = c("red"),
+      show_mean = TRUE, show_points = TRUE, with_ci = TRUE) 
+  print(mean)
+  
+  mean_points <- plot(set, title = "Mean ONA Scores") %>%
+    units(
+      points = set$points[set$points$classroom == "5",],
+      points_color = c("orange"),
+      show_mean = TRUE, show_points = FALSE, with_ci = TRUE
+    ) %>%
+    units (
+      points = set$points[set$points$classroom == "CE2",],
+      points_color = c("green"),
+      show_mean = TRUE, show_points = FALSE, with_ci = TRUE
+    ) %>%
+    units (
+      points = set$points[set$points$classroom == "CM1",],
+      points_color = c("blue"),
+      show_mean = TRUE, show_points = FALSE, with_ci = TRUE
+    ) %>%
+    units (
+      points = set$points[set$points$classroom == "CM2",],
+      points_color = c("purple"),
+      show_mean = TRUE, show_points = FALSE, with_ci = TRUE
+    )
+  print(mean_points)
+  
+  class_5 <- plot(set, title = "Classroom 5 plot") %>%
+    edges(
+      weights = set$line.weights[set$line.weights$classroom == "5",],
+      edge_size_multiplier = 2,
+      edge_color = c("orange")) %>%
+    nodes(
+      # node_size_multiplier = 0.5,
+      self_connection_color = c("orange")) %>%
+    units(
+      points=set$points[set$points$classroom == "5",],
+      points_color = c("orange") ,
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE) 
+  print(class_5)
+  
+  class_CE2 <- plot(set, title = "Classroom CE2 plot") %>%
+    edges(
+      weights = set$line.weights[set$line.weights$classroom == "CE2",],
+      edge_size_multiplier = 2,
+      edge_color = c("green")) %>%
+    nodes(
+      # node_size_multiplier = 0.5,
+      self_connection_color = c("green")) %>%
+    units(
+      points=set$points[set$points$classroom == "CE2",],
+      points_color = c("green") ,
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE) 
+  print(class_CE2)
+  
+  class_CM1 <- plot(set, title = "Classroom CM1 plot") %>%
+    edges(
+      weights = set$line.weights[set$line.weights$classroom == "CM1",],
+      edge_size_multiplier = 2,
+      edge_color = c("blue")) %>%
+    nodes(
+      # node_size_multiplier = 0.5,
+      self_connection_color = c("blue")) %>%
+    units(
+      points=set$points[set$points$classroom == "CM1",],
+      points_color = c("blue") ,
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE) 
+  print(class_CM1)
+  
+  class_CM2 <- plot(set, title = "Classroom CM2 plot") %>%
+    edges(
+      weights = set$line.weights[set$line.weights$classroom == "CM2",],
+      edge_size_multiplier = 2,
+      edge_color = c("purple")) %>%
+    nodes(
+      # node_size_multiplier = 0.5,
+      self_connection_color = c("purple")) %>%
+    units(
+      points=set$points[set$points$classroom == "CM2",],
+      points_color = c("purple") ,
+      show_mean = FALSE, show_points = TRUE, with_ci = FALSE) 
+  print(class_CM2)
+}
